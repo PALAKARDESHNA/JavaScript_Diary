70ecd3fe4a53e0a5f022c94091ada4e3fc9498fb
ardeshna_palak_11 <palakardeshna@gmail.com>
2025-07-29 11:07:56 +0530

    variable commit

---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 123; 
+let accountEmail = "palakardeshna@gmail.com";
+var accountpasword = "12345678";
+
+accountcity = "surat";
+
+let accountstate;
+
+// accountEmail = "pllll@gmail.com";
+// accountpasword = "123e";
+// accountcity = "mumbai";
+
+
+console.log(accountstate);
+
