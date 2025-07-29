d0033729595e4acb0057e9f3fec446ebb26f1dca
ardeshna_palak_11 <palakardeshna@gmail.com>
2025-07-29 12:02:00 +0530

    conversion

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all code as strict JavaScript - js code as newer version
+
+// alert(3+3)
+
+
+let Fullname = "palak";
+
+console.log(typeof "Fullname"); // undefined, because Fullname is not declared with var, let or const
+
